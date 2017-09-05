e872281c4b096ef1ea7b916665a5c8183390130b
shrey <shrey.saroch@gmail.com>
2017-09-05 22:58:29 +0530

    deleting react project

--- src/main/resources/frontend/src/utils/product.js
+++
@@ -1,9 +0,0 @@
-export function hasStock(product) {
-  switch (product.stock_type) {
-    case 'status':
-      return (product.stock_status === 'in_stock');
-    default:
-      console.warn('Unhandled stock type, add it to utils/product.js', product.stock_type);
-      return true;
-  }
-}
