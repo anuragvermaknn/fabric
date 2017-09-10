6f5823cdea5409b852955e92a4918bf62d65785d
shrey <shrey.saroch@gmail.com>
2017-09-10 16:39:10 +0530

    changing params

--- src/main/resources/static/prod.js
+++ src/main/resources/static/prod.js
@@ -1,11 +1,12 @@
 /**
- * Created by nikhil on 10/17/16.
+ * Created by shrey on 10/09/17.
  */
 module.exports = {
-    storefrontUrl: 'https://store.adda247.com',
-    couponUrl: 'https://couponapi.adda247.com',
-    userUrl: 'https://userapi.adda247.com',
-    assetsURLPrefix: 'https://storecdn.adda247.com',
-    keyboardEnabled: false,
-    videoPdpDataUrl : 'https://storecdn.adda247.com/digital-course/prod/'
+    storefrontUrl: '',
+    couponUrl: '',
+    userUrl: '',
+    assetsURLPrefix: '',
+    keyboardEnabled: true,
+    videoPdpDataUrl: ''
+
 };
