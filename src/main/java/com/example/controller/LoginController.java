d7a63c35869bdf13c0de6340e8e236e08f730dd3
NAVNEETSINGH1 <navneet.singh@careerpower.in>
2017-09-13 12:25:06 +0530

    changes for fabric

--- src/main/java/com/example/controller/LoginController.java
+++ src/main/java/com/example/controller/LoginController.java
@@ -22,6 +22,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -38,6 +39,7 @@ import com.example.service.IItemService;
 import com.example.service.UserService;
 
 @Controller
+@CrossOrigin(value = "*", origins = "*")
 public class LoginController
 {
 
