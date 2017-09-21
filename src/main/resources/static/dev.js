d325b066c832d4a43b0de9c32a507627aecd947b
Shrey Saroch <shrey.saroch@adda247.com>
2017-09-21 11:27:37 +0530

    serving images from cdn

--- src/main/resources/static/dev.js
+++ src/main/resources/static/dev.js
@@ -5,7 +5,7 @@ module.exports = {
     storefrontUrl: 'https://store.adda247.com',
     couponUrl: 'https://couponapi.adda247.com',
     userUrl: 'https://userapi.adda247.com',
-    assetsURLPrefix: 'https://storecdn.adda247.com',
+    assetsURLPrefix: 'https://res.cloudinary.com/drd7xdko2/image/upload/v1505971532',
     keyboardEnabled: true,
     videoPdpDataUrl: 'https://storecdn.adda247.com/digital-course/prod/'
 
