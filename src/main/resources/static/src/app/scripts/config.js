f2b9845eaad266686dc58b40d1e9da3504620d04
Shrey Saroch <shrey.saroch@adda247.com>
2017-09-24 23:14:43 +0530

    deleting old assets

--- src/main/resources/static/src/app/scripts/config.js
+++ src/main/resources/static/src/app/scripts/config.js
@@ -12,7 +12,7 @@ angular.module('config', [])
 
 .constant('assetsURLPrefix', 'https://res.cloudinary.com/drd7xdko2/image/upload/v1505971532')
 
-.constant('keyboardEnabled', true)
+.constant('keyboardEnabled', false)
 
 .constant('videoPdpDataUrl', 'https://storecdn.adda247.com/digital-course/prod/')
 
